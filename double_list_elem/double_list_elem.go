f61911ecfa2630264b8fd9c942542d726fd76b62
Nicolas Valcarcel <nvalcarcel@gmail.com>
2015-10-18 11:05:52 -0500

    re-organize code in a saner way

--- double_list_elem/double_list_elem.go
+++
@@ -1,7 +0,0 @@
-package double_list_elem
-
-type DoubleListElem struct {
-	Data interface{}
-	Next *DoubleListElem
-	Prev *DoubleListElem
-}
--- simple_list_elem/simple_list_elem.go
+++
@@ -1,6 +0,0 @@
-package simple_list_elem
-
-type SimpleListElem struct {
-	Data interface{}
-	Next *SimpleListElem
-}
