214d6ed6a8f7ccde0a0c56f31e0f0754496fbe43
Nicolas Valcarcel <nvalcarcel@gmail.com>
2015-11-24 21:54:06 -0500

    rewrite stack with inheritance

--- stacks/stack.go
+++ stacks/stack.go
@@ -1,5 +1,7 @@
 package stacks
 
+import "github.com/nxvl/mastering-algorithms-with-go/linked_lists"
+
 // Stack item struct, contains an interface to the data and a pointer
 // to the next element in stack
 type StackItem struct {
@@ -10,45 +12,35 @@ type StackItem struct {
 // Stack struct, contains a pointer to the stack item at the top
 // of the stack
 type Stack struct {
-	top  *StackItem
-	size int
+	linked_list.SimpleLinkedList
 }
 
 // Pushes the data passed into the stack, optionally returns an error
 func (stack *Stack) Push(data interface{}) error {
-	new_elem := &StackItem{
-		data: data,
-		next: stack.top,
-	}
-	stack.top = new_elem
-	stack.size++
-	return nil
+	return stack.InsNext(nil, data)
 }
 
 // Pops the element at the top of the list, returns the data at the top
 // of the stack and an optional error. Removes the element from the top.
 func (stack *Stack) Pop() (interface{}, error) {
-	if stack.Size() == 0 {
-		return nil, ErrEmptyStack
+	data, err := stack.RemNext(nil)
+	switch {
+	case err == nil:
+		return data, nil
+	case err == linked_list.ErrEmptyList:
+		return data, ErrEmptyStack
+	default:
+		return data, err
 	}
-	data := stack.top.data
-	stack.top = stack.top.next
-	stack.size--
-	return data, nil
 }
 
 // Peeks the element at the top of the list, returns the data at the top
 // of the stack and an optional error. Does not remove the element at
 // the top
 func (stack Stack) Peek() (interface{}, error) {
-	if stack.top == nil {
+	if stack.Head() == nil {
 		return nil, ErrEmptyStack
 	} else {
-		return stack.top.data, nil
+		return stack.Data(stack.Head()), nil
 	}
-}
-
-// Returns the number of elements in the stack
-func (stack Stack) Size() int {
-	return stack.size
 }
